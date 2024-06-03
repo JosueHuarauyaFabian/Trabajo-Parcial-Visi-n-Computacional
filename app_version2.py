62a4118b3fa7b5d0ff11bbd5daa4cfdf3d2eb1d4
limAle <118579154+alexandralima1602@users.noreply.github.com>
2024-06-02 22:31:46 -0500

    Update app_version2.py

--- app_version2.py
+++ app_version2.py
@@ -1,1 +1,15 @@
+import tempfile
+import os
+import zipfile
+import time
+from flask import Flask, request, redirect, render_template, send_file
+from skimage import io
+import base64
+import glob
+import numpy as np
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+import json
 
+app = Flask(__name__)
