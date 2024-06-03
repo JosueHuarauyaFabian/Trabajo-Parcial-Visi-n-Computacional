1051eac4805199280edc6e46090f66be23444ada
limAle <118579154+alexandralima1602@users.noreply.github.com>
2024-06-02 22:32:21 -0500

    Update app_version2.py

--- app_version2.py
+++ app_version2.py
@@ -13,3 +13,14 @@ from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 import json
 
 app = Flask(__name__)
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+
+# Lee las credenciales desde las variables de entorno
+credentials_json = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+if credentials_json:
+    credentials_info = json.loads(credentials_json)
+    credentials = service_account.Credentials.from_service_account_info(credentials_info, scopes=SCOPES)
+else:
+    raise ValueError("No GOOGLE_APPLICATION_CREDENTIALS found in environment variables")
+
+service = build('drive', 'v3', credentials=credentials)
